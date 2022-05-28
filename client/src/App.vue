8f8d8654f559e9b0f066b0e31d3054b5a4573718
rokasp <rokas.paulikas@gmail.com>
2022-05-28 15:17:36 +0300

    cards

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,26 +1,19 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import CardButton from './components/CardButton.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <div class="bg-gray-100 w-screen h-screen">
+    <div class="container mx-auto font-sans">
+      <div class="grid grid-flow-col grid-rows-1 p-10">
+        <CardButton points=1 />
+        <CardButton points=2 />
+        <CardButton points=3 />
+        <CardButton points=5 />
+        <CardButton points=8 />
+        <CardButton points=13 />
+        <CardButton points=21 />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ client/src/components/CardButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="inline-block">
+    <button class="rounded-md p-2 m-2 border-solid border-2 border-black hover:bg-emerald-500 w-16 h-20 text-2xl">
+      <span class="font-semibold">
+        {{ points }}
+      </span>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      points: Number
+    }
+  }
+</script>
